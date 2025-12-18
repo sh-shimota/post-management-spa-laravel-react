44cb47202072d14d42a83cbba61df7b764f601d6
sh-shimota <sh.shimota@gmail.com>
2025-12-18 12:08:09 +0900

    chore: add frontend files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Layout from "./Pages/Layout";
+import Home from "./Pages/Home";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
